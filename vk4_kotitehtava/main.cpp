f0bdbf7e413cdec27450690587122d55acca619b
SuviLep <c3lesu00@students.oamk.fi>
2024-01-30 11:58:42 +0200

    tehtava 1

---
+++ vk4_kotitehtava/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "asunto.h"
+
+using namespace std;
+
+int main()
+{
+    Asunto olio;
+    olio.maarita(2,100);
+    //olio.laskeKulutus(1);
+
+    cout<<"asunnon kulutus, kun hinta = 1 on "<<olio.laskeKulutus(1,false)<<endl;
+    return 0;
+}
